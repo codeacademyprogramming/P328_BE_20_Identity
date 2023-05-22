81cbd1d883f5e199862806882f5fc0f22094876b
Elchin Aghayev <Elchin@code.local>
2023-05-22 21:38:26 +0400

    project added

---
+++ P328Pustok/P328Pustok/Controllers/AccountController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P328Pustok.Models;
+using P328Pustok.ViewModels;
+
+namespace P328Pustok.Controllers
+{
+    public class AccountController : Controller
+    {
+		private readonly UserManager<AppUser> _userManager;
+		private readonly SignInManager<AppUser> _signInManager;
+
+		public AccountController(UserManager<AppUser> userManager,SignInManager<AppUser> signInManager)
+        {
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(MemberRegisterViewModel registerVM)
+        {
+            if (!ModelState.IsValid) return View();
+
+            if(_userManager.Users.Any(x=>x.UserName == registerVM.UserName))
+            {
+                ModelState.AddModelError("UserName", "UserName is alredy taken");
+                return View();
+            }
+
+			if (_userManager.Users.Any(x => x.Email == registerVM.Email))
+			{
+				ModelState.AddModelError("Email", "Email is alredy taken");
+				return View();
+			}
+
+            AppUser user = new AppUser
+            {
+                FullName = registerVM.FullName,
+                UserName = registerVM.UserName,
+                Email = registerVM.Email,
+                IsAdmin = false
+            };
+
+            var result = await _userManager.CreateAsync(user, registerVM.Password);
+            if (!result.Succeeded)
+            {
+                foreach (var err in result.Errors)
+                    ModelState.AddModelError("", err.Description);
+                return View();
+			}
+
+            await _signInManager.SignInAsync(user,false);
+
+            return RedirectToAction("index", "home");
+		}
+
+        public async  Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("login", "account");
+        }
+
+    }
+}
---
+++ P328Pustok/P328Pustok/Controllers/BookController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using P328Pustok.DAL;
+using P328Pustok.Models;
+using P328Pustok.ViewModels;
+
+namespace P328Pustok.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly PustokContext _context;
+
+        public BookController(PustokContext context)
+        {
+            _context = context;
+        }
+        public IActionResult GetBookDetail(int id)
+        {
+            Book book = _context.Books
+                .Include(x => x.Author)
+                .Include(x => x.BookImages)
+                .Include(x => x.BookTags).ThenInclude(x => x.Tag)
+                .FirstOrDefault(x => x.Id == id);
+
+            if (book == null) return StatusCode(404);
+
+            //return Json(new { book = book });
+            return PartialView("_BookModalPartial", book);
+        }
+
+        public IActionResult AddToBasket(int id)
+        {
+            List<BasketItemCookieViewModel> cookieItems = new List<BasketItemCookieViewModel>();
+
+            BasketItemCookieViewModel cookieItem;
+            var basketStr = Request.Cookies["basket"];
+            if (basketStr != null)
+            {
+                cookieItems = JsonConvert.DeserializeObject<List<BasketItemCookieViewModel>>(basketStr);
+
+                cookieItem = cookieItems.FirstOrDefault(x => x.BookId == id);
+
+                if (cookieItem != null)
+                    cookieItem.Count++;
+                else
+                {
+                    cookieItem = new BasketItemCookieViewModel { BookId = id, Count = 1 };
+                    cookieItems.Add(cookieItem);
+                }
+            }
+            else
+            {
+                cookieItem = new BasketItemCookieViewModel { BookId = id, Count = 1 };
+                cookieItems.Add(cookieItem);
+            }
+
+            Response.Cookies.Append("Basket", JsonConvert.SerializeObject(cookieItems));
+
+            BasketViewModel bv = new BasketViewModel();
+            foreach (var ci in cookieItems)
+            {
+                BasketItemViewModel bi = new BasketItemViewModel
+                {
+                    Count = ci.Count,
+                    Book = _context.Books.Include(x => x.BookImages).FirstOrDefault(x => x.Id == ci.BookId)
+                };
+                bv.BasketItems.Add(bi);
+                bv.TotalPrice += (bi.Book.DiscountPercent > 0 ? (bi.Book.SalePrice * (100 - bi.Book.DiscountPercent) / 100) : bi.Book.SalePrice) * bi.Count;
+            }
+
+            return PartialView("_BasketPartialView", bv);
+        }
+
+        public IActionResult RemoveBasket(int id)
+        {
+            var basketStr = Request.Cookies["basket"];
+            if (basketStr == null)
+                return StatusCode(404);
+
+            List<BasketItemCookieViewModel> cookieItems = JsonConvert.DeserializeObject<List<BasketItemCookieViewModel>>(basketStr);
+
+            BasketItemCookieViewModel item = cookieItems.FirstOrDefault(x => x.BookId == id);
+
+            if (item == null)
+                return StatusCode(404);
+
+            if (item.Count > 1)
+                item.Count--;
+            else
+                cookieItems.Remove(item);
+
+            Response.Cookies.Append("basket", JsonConvert.SerializeObject(cookieItems));
+
+            BasketViewModel bv = new BasketViewModel();
+            foreach (var ci in cookieItems)
+            {
+                BasketItemViewModel bi = new BasketItemViewModel
+                {
+                    Count = ci.Count,
+                    Book = _context.Books.Include(x => x.BookImages).FirstOrDefault(x => x.Id == ci.BookId)
+                };
+                bv.BasketItems.Add(bi);
+                bv.TotalPrice += (bi.Book.DiscountPercent > 0 ? (bi.Book.SalePrice * (100 - bi.Book.DiscountPercent) / 100) : bi.Book.SalePrice) * bi.Count;
+            }
+
+            return PartialView("_BasketPartialView", bv);
+        }
+
+        public IActionResult ShowBasket()
+        {
+            var basket = new List<BasketItemCookieViewModel>();
+            var basketStr = Request.Cookies["basket"];
+
+            if (basketStr != null)
+                basket = JsonConvert.DeserializeObject<List<BasketItemCookieViewModel>>(basketStr);
+
+            return Json(new { basket });
+        }
+    }
+}
